d84c0e9013dd8d17062991c2b763dccad985bf9b
shaoshengsong <30892500+shaoshengsong@users.noreply.github.com>
2022-10-11 15:27:35 +0800

    Update YOLOv5Detector.h

--- detector/YOLOv5/include/YOLOv5Detector.h
+++ detector/YOLOv5/include/YOLOv5Detector.h
@@ -2,6 +2,8 @@
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
+#include <fstream>
+#include <sstream>
 class detect_result
 {
 public:
@@ -17,12 +19,14 @@ class YOLOv5Detector
 public:
     void init(std::string onnxpath);
     void detect(cv::Mat & frame, std::vector<detect_result> &result);
+     std::vector<std::string> classes_;
+     void draw_frame(cv::Mat & frame, std::vector<detect_result> &results);
 private:
 
     cv::dnn::Net net;
 
     const float confidence_threshold_ =0.25f;
-    const float nms_threshold_ = 0.45f;
+    const float nms_threshold_ = 0.4f;
 
     const int model_input_width_ = 640;
     const int model_input_height_ = 640;
